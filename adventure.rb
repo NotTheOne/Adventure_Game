5e26a28d87754703c86d848036020e356c7621e9
siameseguy <psyhakhom@gmail.com>
2012-08-28 15:30:31 -0400

    Revert "added game engine"
    This reverts commit 9961562804a8f470e6c19f010efefe68f65a68e9.

--- adventure.rb
+++ adventure.rb
@@ -1,19 +1,51 @@
-class Engine
-  def start
-    Proc.new do
-      main_hallway
-    end
+class Imp
+  attr_accessor :hp, :defense, :atk
+  def intialize(hp, defense, atk)
+    @hp = hp
+    @defense = defense
+    @atk = atk
   end
 end
 
-class Game < Engine
-  def main_hallway
-    puts 'You are in the main hallway.'
+class Hero
+  attr_accessor :hp, :defense, :atk
+  def intialize(hp, defense, atk)
+    @hp = hp
+    @defense = defense
+    @atk = atk
   end
+  
+  def attack
+    if action == 'hit'
+      puts "The hero attacked."
+    else 
+      puts 'what\'s your next move?'
 end
+  
 
-new_game = Game.new
-new_game.start.call
+imp1 = Imp.new
+imp1.hp = rand(100)
+imp1.defense = 50
+imp1.atk = rand(100)
+
+hero1 = Hero.new
+hero1.hp = rand(100)
+hero1.defense = 100
+hero1.atk = rand(100)
+
+puts imp1.hp
+puts hero1.hp
 
 
-    
+#hp = rand(100)
+#defense = 100
+
+#if hp < 30
+#  puts "Your health is #{hp}.  You are in critial condition."
+#end
+#if hp == 0
+#  puts "Your health is at zero.  You are now dead."
+#end
+
+#puts "The imp attacts."
+
